42186f5a7ab1d7a08b58d9b6d295426787771cfe
Antoine Leblanc <antoine.leblanc@corp.ovh.com>
2018-08-20 17:13:11 +0200

    chore(eslint): update eslint scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = merge(common , process.env.WEBPACK_SERVE ? require("./webpack.dev.js") : require("./webpack.prod.js"))
+module.exports = merge(common, process.env.WEBPACK_SERVE ? require('./webpack.dev.js') : require('./webpack.prod.js'));
